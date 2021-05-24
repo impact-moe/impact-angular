ddcd3c5f891156c3a77f4eb7c0234bc5928914af
vikarina <52547470+vikarina@users.noreply.github.com>
2021-05-24 16:49:47 -0700

    refactor: create component for loading image (#9)
    Centralize the loading image + applied stylings.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,66 +1,66 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { NavComponent } from './components/nav/nav.component';
-import { CharacterComponent } from './components/character/character.component';
-import { WeaponListComponent } from './components/weapon-list/weapon-list.component';
-import { CharacterListComponent } from './components/character-list/character-list.component';
-import { ArtifactListComponent } from './components/artifact-list/artifact-list.component';
-import { DropdownContentCardComponent } from 'src/app/components/dropdown-content-card/dropdown-content-card.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { StatisticsComponent } from './components/statistics/statistics.component';
-import { ArtifactComponent } from './components/artifact/artifact.component';
-import { WeaponComponent } from './components/weapon/weapon.component';
-import { RouteButtonComponent } from './components/route-button/route-button.component';
-import { MoeRarityComponent } from './components/rarity/rarity.component';
-
-import { ImpactService } from './services/impact.service';
-import { UtilityService } from './services/utility.service';
-
-import { LinkHandlerDirective } from './directives/link-handler/link-handler.directive';
-
-import { MultiplierPipe } from './pipes/multiplier.pipe';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ArtifactComponent } from './components/artifact/artifact.component';
+import { ArtifactListComponent } from './components/artifact-list/artifact-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { CharacterComponent } from './components/character/character.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { DropdownContentCardComponent } from './components/dropdown-content-card/dropdown-content-card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ImpactService } from './services/impact.service';
+import { LinkHandlerDirective } from './directives/link-handler/link-handler.directive';
+import { LoadingImageComponent } from './components/loading-image/loading-image.component';
+import { LoginComponent } from './components/login/login.component';
+import { MoeRarityComponent } from './components/rarity/rarity.component';
+import { MultiplierPipe } from './pipes/multiplier.pipe';
+import { NavComponent } from './components/nav/nav.component';
+import { NgModule } from '@angular/core';
+import { RouteButtonComponent } from './components/route-button/route-button.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { UtilityService } from './services/utility.service';
+import { WeaponComponent } from './components/weapon/weapon.component';
+import { WeaponListComponent } from './components/weapon-list/weapon-list.component';
 
 const baseUrlProvider = {
   provide: 'BASE_URL',
   useFactory: () => document.getElementsByTagName('base')[0].href,
-  deps: []
+  deps: [],
 };
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NavComponent,
+    ArtifactComponent,
+    ArtifactComponent,
+    ArtifactListComponent,
     CharacterComponent,
     CharacterListComponent,
-    WeaponListComponent,
-    ArtifactListComponent,
     DropdownContentCardComponent,
-    LoginComponent,
-    SignupComponent,
     FooterComponent,
+    HomeComponent,
+    LinkHandlerDirective,
+    LinkHandlerDirective,
+    LoadingImageComponent,
+    LoginComponent,
+    MoeRarityComponent,
+    MultiplierPipe,
+    NavComponent,
+    RouteButtonComponent,
+    RouteButtonComponent,
+    SignupComponent,
     StatisticsComponent,
     WeaponComponent,
-    ArtifactComponent,
-    LinkHandlerDirective,
-    RouteButtonComponent,
-    MoeRarityComponent,
-    MultiplierPipe
+    WeaponListComponent,
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
   ],
   providers: [ImpactService, UtilityService, baseUrlProvider],
   bootstrap: [AppComponent],
